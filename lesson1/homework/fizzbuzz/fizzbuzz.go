0567864b102f8e1a381e2c35d9b91787667e03f4
i.v.vasilenko <i.v.vasilenko@tinkoff.ru>
2023-12-19 19:25:34 +0300

    Homework 1 is done

--- lesson1/homework/fizzbuzz/fizzbuzz.go
+++ lesson1/homework/fizzbuzz/fizzbuzz.go
@@ -1,6 +1,17 @@
 package fizzbuzz
 
+import (
+	"fmt"
+)
+
 func FizzBuzz(i int) string {
-	// TODO
-	return ""
+	if i/3 == 1 {
+		return "Fizz"
+	} else if i/5 == 1 {
+		return "Buzz"
+	} else if i/15 == 1 {
+		return "FizzBuzz"
+	} else {
+		return fmt.Sprint(i)
+	}
 }
